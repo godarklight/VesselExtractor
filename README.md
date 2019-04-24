75cbe688bf7283a287ca1ad56cf4edef2d6039ff
Christopher Andrews <godarklight@gmail.com>
2019-04-25 01:06:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+For some reason KSP can't load big config nodes, so there's this program that uses my implementation of config nodes from years ago.
---
+++ VesselExtractor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using ConfigNodeParser;
+
+namespace VesselExtractor
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            if (!File.Exists("persistent.sfs"))
+            {
+                Console.WriteLine("Place persistent.sfs next to this program.");
+                return;
+            }
+            Console.WriteLine("Extracting vessels!");
+            ConfigNode[] vessels = ConfigNodeReader.FileToConfigNode("persistent.sfs").GetNode("GAME").GetNode("FLIGHTSTATE").GetNodes("VESSEL");
+            if (Directory.Exists("Vessels"))
+            {
+                Directory.Delete("Vessels", true);
+            }
+            Directory.CreateDirectory("Vessels");
+            foreach (ConfigNode vesselnode in vessels)
+            {
+                string vesselID = ConvertConfigStringToGUIDString(vesselnode.GetValue("pid"));
+                string vesselName = vesselnode.GetValue("name");
+                Console.WriteLine("Saving vessel: " + vesselName);
+                string fileText = ConfigNodeWriter.ConfigNodeToString(vesselnode);
+                File.WriteAllText(Path.Combine("Vessels", vesselID + ".txt"), fileText);
+            }
+        }
+
+        public static string ConvertConfigStringToGUIDString(string configNodeString)
+        {
+            if (configNodeString == null || configNodeString.Length != 32)
+            {
+                return null;
+            }
+            string[] returnString = new string[5];
+            returnString[0] = configNodeString.Substring(0, 8);
+            returnString[1] = configNodeString.Substring(8, 4);
+            returnString[2] = configNodeString.Substring(12, 4);
+            returnString[3] = configNodeString.Substring(16, 4);
+            returnString[4] = configNodeString.Substring(20);
+            return String.Join("-", returnString);
+        }
+    }
+}
